e2222f973eafec46d9ebbf953e681eb2b194be5a
Caroline Reis <caroline.s_reis@icloud.com>
2020-06-17 23:24:01 -0300

    Modificações na interface

--- main.py
+++ main.py
@@ -12,19 +12,6 @@ from PIL import Image
 from io import BytesIO
 import time
 
-def decode_base64(data, altchars=b'+/'):
-    """Decode base64, padding being optional.
-
-    :param data: Base64 data as an ASCII byte string
-    :returns: The decoded byte string.
-
-    """
-    data = re.sub(rb'[^a-zA-Z0-9%s]+' % altchars, b'', data)  # normalize
-    missing_padding = len(data) % 4
-    if missing_padding:
-        data += b'='* (4 - missing_padding)
-    return base64.b64decode(data, altchars)
-
 def data_uri_to_cv2_img(uri):
     # nparr = np.fromstring(uri.decode('base64'), np.uint8)
     img = uri.split(',')[1]
