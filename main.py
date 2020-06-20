b684ad74d827a8a9941114d4f57bd3a827e1e0e6
Caroline Reis <caroline.s_reis@icloud.com>
2020-06-20 15:32:37 -0300

    Correções

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import redirect, make_response, request
 from flask import render_template
 from rhme import api
+from rhme.helpers.exceptions import GrammarError, LexicalError, SintaticError
 import os
 import inspect
 import json
@@ -16,7 +17,7 @@ app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe()))))
 
-api.config().set_app_debug_mode_image('disabled')
+# api.config().set_app_debug_mode_image('disabled')
 
 @app.route('/', methods=['GET'])
 def index():
@@ -24,9 +25,9 @@ def index():
 
 @app.route('/ajax/recognize', strict_slashes=False, methods=['POST', 'OPTIONS'])
 def recognize():
-    print('hi')
-    latex = ""
 
+    latex = ""
+    error=True
     try:
         data = request.get_json()
         img_data = data['image']
@@ -44,9 +45,16 @@ def recognize():
         latex, modified_img = hme_recognizer.recognize(img)
         hme_recognizer = None
     except Exception as e:
+        if hasattr(e, 'data'):
+            if 'latex_string_original' in e.data:
+                expression = e.data['latex_string_original']
+                error=True
         print(e)
 
-    return json.dumps({'latex': latex})
+    return json.dumps({
+        'latex': latex,
+        'error': error
+    })
 
 def write_show_image(img, name):
     cv2.imwrite('%s.jpg' % name, img)
--- static/js/recognize.js
+++ static/js/recognize.js
@@ -5,12 +5,19 @@ var myBoard = new DrawingBoard.Board('zbeubeu', {
         { Navigation: { back: false, forward: false } },
         { DrawingMode: { filler: false } }
     ],
-    webStorage: 'local'
+    // webStorage: 'local'
+    webStorage: false
 });
 
 var reconhecer = document.querySelector("#reconhecer");
+var latexElement = document.querySelector("#latex");
+var errorElement = document.querySelector(".quadro__resultado__error");
 
 reconhecer.addEventListener("click", function() {
+
+    latexElement.classList.remove('--error');
+    errorElement.classList.remove('--show');
+
     if(!reconhecer.classList.contains('--disabled')) {
         reconhecer.classList.add('--disabled');
         var img = myBoard.getImg();
@@ -28,7 +35,11 @@ reconhecer.addEventListener("click", function() {
         })
         .then(function(data) {
             data = JSON.parse(data)
-            document.querySelector("#latex").innerText = data.latex;
+            latexElement.innerText = data.latex;
+            if(data.error) {
+                latexElement.classList.add('--error')
+                errorElement.classList.add('--show');
+            }
         })
         .finally(function() {
             reconhecer.classList.remove('--disabled');
